137c2e14f9ccd2d8581e899343110ec3723a7c6f
Gvtiso Bugashvili <bugashvilig@gmail.com>
2026-05-27 18:10:34 +0400

    update 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,9 @@ export default function RootLayout({
       className={`${playfair.variable} ${inter.variable} h-full`}
       suppressHydrationWarning
     >
-      <body className="flex min-h-full flex-col">{children}</body>
+      <body className="flex min-h-full flex-col" suppressHydrationWarning>
+        {children}
+      </body>
     </html>
   );
 }
