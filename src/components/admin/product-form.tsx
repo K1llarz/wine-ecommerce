737cd059b349453809265229d77519de978b67ba
Gvtiso Bugashvili <bugashvilig@gmail.com>
2026-05-28 15:42:14 +0400

    Remove unused import in product-form

--- src/components/admin/product-form.tsx
+++ src/components/admin/product-form.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Link from "next/link";
 import { useActionState, type ReactNode } from "react";
 import { useFormStatus } from "react-dom";
 import { saveProductAction, type ProductFormState } from "@/app/actions/admin-products";
